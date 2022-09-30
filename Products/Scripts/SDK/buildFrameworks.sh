2b827711f6914ef1032f2c97466adbffb7585aab
Lyy <liuyuanyuan02@agora.io>
2022-09-30 15:51:32 +0800

    [ENH]: add base files

---
+++ Products/Scripts/SDK/buildFrameworks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ../../../../open-cloudclass-ios/Products/Scripts/SDK
+pwd
+sh ./buildFrameworks.sh
+
+cd ../../../../open-proctor-ios/Products/Scripts/SDK
+pwd
+sh ./buildFrameworks.sh
