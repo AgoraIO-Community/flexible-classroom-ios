39c38a6b9ebcd31e4c6ad4ada304654972470caf
cavan <403029552@qq.com>
2025-04-17 18:24:51 +0800

    [ENH]:update version & dependency

--- Products/Scripts/Build/dependency.sh
+++ Products/Scripts/Build/dependency.sh
@@ -20,7 +20,7 @@ AgoraProctorUI_URL="${Artifactory_iOS_URL}/AgoraProctorUI/release_1.0.2/dev/Agor
 
 AgoraWidgets_URL="${Artifactory_iOS_URL}/AgoraWidgets/release_2.8.105/dev/AgoraWidgets_2.8.105.zip"
 
-AgoraEduCore_URL="${Artifactory_iOS_URL}/AgoraEduCore/release_2.8.105/dev/AgoraEduCore_2.8.105.zip"
+AgoraEduCore_URL="${Artifactory_iOS_URL}/AgoraEduCore/release_2.8.107/dev/AgoraEduCore_2.8.107.zip"
 AgoraWidget_URL="${Artifactory_iOS_URL}/AgoraWidget/release_2.8.105/dev/AgoraWidget_2.8.0.zip"
 AgoraFoundation_URL="${Artifactory_iOS_URL}/AgoraFoundation/feature_3.4.0_rx/dev/AgoraFoundation_3.4.0_dev.zip"
 AgoraUIBaseViews_URL="${Artifactory_iOS_URL}/AgoraUIBaseViews/release_2.8.105/dev/AgoraUIBaseViews_2.8.101.zip"
