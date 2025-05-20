6cde0bc4f436dcf9f4075576a16af983c02e4bdc
cavan <403029552@qq.com>
2025-05-20 19:47:37 +0800

    [FEAT]:add publish sh

---
+++ Products/Scripts/Publish/publish.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# cd this file path
+cd $(dirname $0)
+echo pwd: `pwd`
+
+Branch_Name=$1
+
+if [ -z "$Branch_Name" ]; then
+    echo "Branch_Name is empty"
+    exit 1
+fi
+
+# add originGithub
+if git remote -v | grep -q 'originGithub'; then
+    echo "originGithub exist"
+else
+    Git_Hub="git@github.com:AgoraIO-Community/CloudClass-iOS.git"
+
+    git remote add originGithub ${Git_Hub}
+fi
+
+git push originGithub ${Branch_Name}
