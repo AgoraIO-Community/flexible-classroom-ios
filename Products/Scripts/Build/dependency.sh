b12e3ec2785a04bc8497f1a471e4ac0ddecd260c
vitas <lcztt@users.noreply.github.com>
2025-09-03 11:49:33 +0800

    remove repeated variables

--- Products/Scripts/Build/dependency.sh
+++ Products/Scripts/Build/dependency.sh
@@ -13,14 +13,13 @@
 Artifactory_iOS_URL="https://artifactory.agoralab.co/artifactory/AD_repo/aPaaS/iOS"
 
 Version="2.8.112"
-ProctorBranch="release_${Version}"
 Branch="release_${Version}"
 
 AgoraClassroomSDK_iOS_URL="${Artifactory_iOS_URL}/AgoraClassroomSDK_iOS/${Branch}/dev/AgoraClassroomSDK_iOS_${Version}.zip"
 AgoraEduUI_URL="${Artifactory_iOS_URL}/AgoraEduUI/${Branch}/dev/AgoraEduUI_${Version}.zip"
 
-AgoraProctorSDK_URL="${Artifactory_iOS_URL}/AgoraProctorSDK/${ProctorBranch}/dev/AgoraProctorSDK_${Version}.zip"
-AgoraProctorUI_URL="${Artifactory_iOS_URL}/AgoraProctorUI/${ProctorBranch}/dev/AgoraProctorUI_${Version}.zip"
+AgoraProctorSDK_URL="${Artifactory_iOS_URL}/AgoraProctorSDK/${Branch}/dev/AgoraProctorSDK_${Version}.zip"
+AgoraProctorUI_URL="${Artifactory_iOS_URL}/AgoraProctorUI/${Branch}/dev/AgoraProctorUI_${Version}.zip"
 
 AgoraWidgets_URL="${Artifactory_iOS_URL}/AgoraWidgets/${Branch}/dev/AgoraWidgets_${Version}.zip"
 
